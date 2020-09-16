b7dc0c7d5a72dd2042f715d3caa7a8dd64e4c812
gueguet57 <gaetan.verdinpol@gmail.com>
2020-09-16 17:49:59 +0200

    add fetching all pokemon and add to cache

--- js/main.js
+++ js/main.js
@@ -3,3 +3,75 @@ if ('serviceWorker' in navigator) {
     .then((resp) => console.log("Service Worker correctly registered ! ", resp))
     .catch((error) => console.log("Error with Service Worker registration ! ",error))
 }
+
+
+// retrieve pokemon data
+var apiData = {
+  url: 'https://pokeapi.co/api/v2/',
+  type: 'pokemon',
+  id: '1'
+}
+
+
+const getPokemonById = async (id) => {
+  const urlPokeData = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const urlPokePic = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+
+  Promise.all([fetch(urlPokeData), fetch(urlPokePic)]).then((values) => {
+    console.log(values);
+  });
+
+
+
+  // const res = await fetch(url);
+  // const pokemon = await res.json();
+
+};
+
+function getData() {
+  let firstAPICall = fetch("https://api.endpoint.com/first");
+  let secondAPICall = fetch("https://api.endpoint.com/second");
+
+
+}
+
+
+
+// get all pokemon at the beginning to be able to retrieve the info offline via cache storage //
+const getAllPokemon = async () => {
+  for (let i = 1; i <= 151; i++) {
+    await getPokemonById(i);
+  }
+} 
+
+
+function test() {
+  let pokeNumber = document.getElementById("pokemon_number").value;
+  console.log(pokeNumber);
+  apiData.id = pokeNumber;
+  
+  let { url, type, id } = apiData;
+  let apiUrl = `${url}${type}/${id}`
+
+  fetch(apiUrl)
+    .then((data) => data.json())
+    .then((poke) => displayPoke(poke))
+    .catch((err) => console.log(err))
+}
+
+
+const displayPoke = (data) => {
+  console.log(data);
+  const html = `
+    <div class="name">${data.name}</div>
+    <img src=${data.sprites.front_default}>
+    <div class="details">
+      <span>Height : ${data.height}</span>
+    </div>
+  `
+
+  const pokeDiv = document.querySelector('.pokemon');
+  pokeDiv.innerHTML = html;
+}
+
--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
-const staticCache = 'site-static-v1';
-const dynamicCache = 'site-dynamic-v1';
+const staticCache = 'site-static-v4';
+const pokemonCache = 'pokemon-dynamic-v4';
 
 // here we need to store request urls !
 const assets = [
   '/',
   '/index.html',
   '/css/style.css',
-  '/js/main.js'
+  '/js/main.js',
+  '/manifest.json',
+  'img/dresseur_16x16.ico',
+  'img/icons/pokedex_80x80.png',
+  'img/icons/pokedex_512x512.png',
+  'img/icons/maskable_icon.png',
 ];
+
+
 
 // if SW is installed
 self.addEventListener('install', evt => {
   // need to be sure caching is done before the install event stop !
   evt.waitUntil(
     caches.open(staticCache).then(cache => {
+      // getAllPokemon(); // after we cached static file --> cache pokemon info 
       console.log("caching assets !");
       return cache.addAll(assets);
     })
@@ -33,16 +41,28 @@ self.addEventListener('activate', evt => {
   )
 })
 
-// fetch event
+
+// we want to cache all pokemon informations once and be able to retrieve those offline 
+// self.addEventListener("fetch", event => {
+//   if (event.request.url.includes("/api/")) {
+//     console.log("yeah yeah"); // test
+//   } else {
+//     console.log("euh...");
+//   }
+// });
+
+
+
+// fetch event --> if a file was cached and we're offline --> return this page
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then((cacheRes) => { // if cachesRes is empty --> redo the fetch request and store in dynamic cache
-      return cacheRes || fetch(evt.request).then((fetchRes) => {
-        return caches.open(dynamicCache).then(cache => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(pokemonCache).then(cache => {
           cache.put(evt.request.url, fetchRes.clone());
           return fetchRes;
         })
-      })
+      });    
     })
   )
 });
